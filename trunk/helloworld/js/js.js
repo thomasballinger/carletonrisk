024412b68ad71dcc54ab44754cf917bea48cc3d2
ThomasBallinger <ThomasBallinger@cafdd4e1-b3f3-efa3-cc62-87e5a30f52c8>
2010-04-08 21:52:19 +0000

    added jquery.js (unnecessarily - in the code we grab Google's copy instead) and played with javascript a lot The new plan is to use javascript for this app

--- trunk/helloworld/js/js.js
+++ trunk/helloworld/js/js.js
@@ -1,2 +1,13 @@
-var myVariable = 'Hello';
-window.alert(myVariable);
+function sayHello(){
+    var myVariable = 'Hello';
+    window.alert(myVariable);
+}
+
+function getDetails(obj){
+    clickX = window.event.x-obj.offsetLeft;
+    clickY = window.event.y-obj.offsetTop;
+    window.alert(clickX+'');
+    window.alert(clickY+'');
+}
+
+sayHello()
